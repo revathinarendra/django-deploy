86c5e438d2db94436932149d7ef1523b0cd97954
KATARI REVATHI <katari.revathi@gmail.com>
2024-05-22 10:46:28 +0530

    message

--- build_files.sh
+++ build_files.sh
@@ -3,10 +3,30 @@
 
 #!/bin/bash
 # Ensure pip is available
-/usr/bin/python3.12.2 -m ensurepip --upgrade
+#/usr/bin/python3.12.2 -m ensurepip --upgrade
 # Upgrade pip
-/usr/bin/python3.12.2 -m pip install --upgrade pip
+#/usr/bin/python3.12.2 -m pip install --upgrade pip
 # Install dependencies from requirements.txt
-/usr/bin/python3.12.2 -m pip install -r requirements.txt
+#/usr/bin/python3.12.2 -m pip install -r requirements.txt
 # Add other necessary build steps below
-python3.12.2 manage.py collectstatic --noinput
+#python3.12.2 manage.py collectstatic --noinput
+#!/bin/bash
+
+# Activate the virtual environment
+source /c/Users/Admin/Desktop/django/django-env/Scripts/activate
+
+# Ensure pip is available
+python -m ensurepip --upgrade
+
+# Upgrade pip
+python -m pip install --upgrade pip
+
+# Install dependencies from requirements.txt
+python -m pip install -r requirements.txt
+
+# Run Django management commands
+python manage.py collectstatic --noinput
+python manage.py migrate
+
+# Deactivate the virtual environment (optional)
+deactivate
